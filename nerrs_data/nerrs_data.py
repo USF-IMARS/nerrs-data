823afe07ca8e16eadb31e9405733c73d2f26406b
Tylar <murray.tylar@gmail.com>
2024-04-07 11:26:23 -0400

    make fn return df & none of influx stuff

--- nerrs_data/nerrs_data.py
+++ nerrs_data/nerrs_data.py
@@ -1,11 +1,10 @@
 """Main module."""
 
-import os
+import xml.etree.ElementTree as ET
 
 import pandas as pd
-import requests
 from suds.client import Client
-import xml.etree.ElementTree as ET
+
 
 def getData(station_code, param_name):
     """
@@ -31,43 +30,14 @@ def getData(station_code, param_name):
         # Append if data row contains exactly two elements (date and value)
         if len(data_row) == 2:
             data_rows.append(data_row)
- 
+
     data_rows = data_rows[1:]  # rm duplicated header row
-            
+
     # Convert the extracted data into a pandas DataFrame
     df = pd.DataFrame(data_rows, columns=['DateTimeStamp', 'Sal'])
-    print(df)
-    
-    # write the data to a csv file
-    df.to_csv("./datafile.csv")
 
-    exit()
-    
-    # === upload the data
-
-    UPLOADER_HOSTNAME = os.environ["UPLOADER_HOSTNAME"]
-    if UPLOADER_HOSTNAME.endswith('/'):  # rm possible trailing /
-        UPLOADER_HOSTNAME = UPLOADER_HOSTNAME[:-1]
-    UPLOADER_ROUTE = UPLOADER_HOSTNAME + "/submit/sat_image_extraction"
-
-    url = UPLOADER_ROUTE  # Replace this with your actual URL
-    data = {
-        'measurement': 'nerrs_met_data',
-        'tag_set': 'station_code=SPOT30987C',
-        'fields': 'Sal',
-        'time_column': 'utc_timestamp',
-    }
-
-    files = {'file': ('datafile.csv', open('./datafile.csv', 'rb'))}
-
-    response = requests.post(url, data=data, files=files)
-
-    if response.status_code == 200:
-        print("Upload successful")
-    else:
-        print(f"Upload failed with status code {response.status_code}")
-        print(response.text)
-        raise ValueError(response.text)
+    return df
 
 
-getData("acespwq", "Sal")
+# test
+# getData("acespwq", "Sal")
